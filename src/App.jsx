6abcde4e0546deb536e91f61ed9d22dbab3af698
safarasd-os <tilen.lorenci12@gmail.com>
2025-07-27 11:57:33 +0200

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,27 @@
 import { Dashboard, HomeLayout, MovieFlix } from "./pages";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <HomeLayout />,
+      children: [
+        {
+          index: true,
+          element: <Dashboard />,
+        },
+        {
+          path: "MovieFlix",
+          element: <MovieFlix />,
+        },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <HomeLayout />,
-    children: [
-      {
-        index: true,
-        element: <Dashboard />,
-      },
-      {
-        path: "MovieFlix",
-        element: <MovieFlix />,
-      },
-    ],
-  },
-]);
+    basename: "/portfolio", // 👈 important for GitHub Pages
+  }
+);
 
 function App() {
   return <RouterProvider router={router} />;
